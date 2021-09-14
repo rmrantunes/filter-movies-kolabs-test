3a625704b011db85356c0db71932be63ef193a4c
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-14 16:28:35 -0400

    feat: add GlobalStyle component to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import { AppProps } from 'next/app'
+import GlobalStyle from 'styles/GlobalStyle'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }

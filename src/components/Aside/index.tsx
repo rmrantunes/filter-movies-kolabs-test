4b6d074964cbb3416a1378fd8f043b21e58ac68b
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 10:52:23 -0400

    feat: add FilterButtons to Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,12 +1,47 @@
+import FilterButton from 'components/FilterButton'
 import * as S from './styles'
+import { useSearch } from 'hooks/useSearch'
 
 export default function Aside() {
+  const { handleFilter, filter } = useSearch()
+
   return (
     <S.Aside>
-      <S.Header>
-        <h2>Resultado da Busca</h2>
-      </S.Header>
-      <S.Nav></S.Nav>
+      <S.Filters>
+        <S.Header>
+          <h2>Resultado da Busca</h2>
+        </S.Header>
+        <S.Nav>
+          <FilterButton
+            onClick={() => ({})}
+            filter="multi"
+            isActive={filter === 'multi'}
+          >
+            Tudo
+          </FilterButton>
+          <FilterButton
+            onClick={handleFilter}
+            filter="movie"
+            isActive={filter === 'movie'}
+          >
+            Filmes
+          </FilterButton>
+          <FilterButton
+            onClick={handleFilter}
+            filter="tv"
+            isActive={filter === 'tv'}
+          >
+            Séries
+          </FilterButton>
+          <FilterButton
+            onClick={handleFilter}
+            filter="person"
+            isActive={filter === 'person'}
+          >
+            Pessoas
+          </FilterButton>
+        </S.Nav>
+      </S.Filters>
     </S.Aside>
   )
 }

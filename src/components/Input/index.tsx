ba535b07bae43e759cd4862daedaf243d8c55a8e
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 10:53:54 -0400

    feat: implement useSearch in Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,21 @@
+import { useSearch } from 'hooks/useSearch'
+import React from 'react'
 import * as S from './styles'
 
 export default function Input() {
+  const { handleSearch, searchText, setSearchText } = useSearch()
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchText(event.target.value)
+  }
+
   return (
-    <S.Wrapper>
-      <S.Input placeholder="Ex: Avengers" />
-    </S.Wrapper>
+    <S.Form onSubmit={handleSearch}>
+      <S.Input
+        placeholder="Ex: Avengers"
+        onChange={handleChange}
+        value={searchText}
+      />
+    </S.Form>
   )
 }

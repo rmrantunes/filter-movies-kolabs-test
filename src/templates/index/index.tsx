02573270d4174afaf52b46a415cb9b60eafb71a5
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 10:57:55 -0400

    feat: add Response types as IndexTemplateProps

--- src/templates/index/index.tsx
+++ src/templates/index/index.tsx
@@ -1,10 +1,23 @@
 import Aside from 'components/Aside'
 import { Container } from 'components/Container'
 import Input from 'components/Input'
+import {
+  MovieResponse,
+  PersonResponse,
+  TvResponse
+} from 'types/themoviedb-response'
 
 import * as S from './styles'
 
-export default function IndexTemplate() {
+export type IndexTemplateProps =
+  | {
+      filter: 'tv'
+      response: TvResponse
+    }
+  | { filter: 'movie'; response: MovieResponse }
+  | { filter: 'person'; response: PersonResponse }
+
+export default function IndexTemplate(props: IndexTemplateProps) {
   return (
     <Container>
       <Input />

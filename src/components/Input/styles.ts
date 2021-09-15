fe7e51f1d3141a742eab67ee1a411363d16e593f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 10:53:30 -0400

    refactor: Input Wrapper as form
    in order to use onSubmit

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div`
+export const Form = styled.form`
   ${({ theme }) => css`
     display: flex;
     padding: 1rem;

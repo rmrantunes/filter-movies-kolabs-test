505c14009dc7561989c7cd29af0cd4344b91d377
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 16:29:35 -0400

    feat: add MovieDBMultiResponse

--- src/types/themoviedb-response.ts
+++ src/types/themoviedb-response.ts
@@ -85,3 +85,46 @@ export interface KnownFor {
   origin_country?: string[]
   original_name?: string
 }
+
+export interface MovieDBMultiResponse {
+  page: number
+  results: MultiResult[]
+  total_pages: number
+  total_results: number
+}
+
+export interface MultiResult {
+  adult?: boolean
+  gender?: number
+  id: number
+  known_for?: MultiResult[]
+  known_for_department?: string
+  media_type: MediaType
+  name?: string
+  popularity?: number
+  profile_path?: null | string
+  backdrop_path?: null | string
+  genre_ids?: number[]
+  original_language?: OriginalLanguage
+  original_title?: string
+  overview?: string
+  poster_path?: null | string
+  release_date?: string
+  title?: string
+  video?: boolean
+  vote_average?: number
+  vote_count?: number
+  first_air_date?: Date
+  origin_country?: string[]
+  original_name?: string
+}
+
+export enum MediaType {
+  Movie = 'movie',
+  Person = 'person',
+  Tv = 'tv'
+}
+
+export enum OriginalLanguage {
+  En = 'en'
+}

f1a391fe4008d2738126d6f360539c84211d43d4
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 17:14:47 -0400

    feat: add resultsToDisplay & mediaTypesCount

--- src/contexts/SearchContext.tsx
+++ src/contexts/SearchContext.tsx
@@ -1,11 +1,19 @@
 import { useRouter } from 'next/router'
-import React, { createContext, useCallback, useState } from 'react'
+import React, { createContext, useCallback, useMemo, useState } from 'react'
 import { MultiResult } from 'types/themoviedb-response'
+
+export type Filters = 'person' | 'tv' | 'movie' | 'all'
 
 export type SearchContextValue = {
   filter: string
   searchText: string
   results: MultiResult[]
+  mediaTypesCount: {
+    all: number
+    movie: number
+    tv: number
+    person: number
+  }
   setResults: React.Dispatch<React.SetStateAction<MultiResult[]>>
   setSearchText: React.Dispatch<React.SetStateAction<string>>
   handleFilter: (filter?: string) => void
@@ -16,24 +24,40 @@ export const SearchContext = createContext({} as SearchContextValue)
 
 export const SearchProvider: React.FC = (props) => {
   const router = useRouter()
-  const [filter, setFilter] = useState('multi')
+  const [filter, setFilter] = useState<Filters>('all')
   const [results, setResults] = useState<MultiResult[]>(null)
   const [searchText, setSearchText] = useState(
     (router.query?.query as string) || ''
   )
 
-  const handleFilter = useCallback(
-    (filter?: string) => {
-      setFilter(filter)
-      router.push({
-        pathname: '/',
-        query: {
-          query: searchText
-        }
-      })
-    },
-    [searchText]
-  )
+  const mediaTypesCount = useMemo(() => {
+    return {
+      all: results?.length || 0,
+      movie:
+        results?.filter((result) => result.media_type === 'movie').length || 0,
+      tv: results?.filter((result) => result.media_type === 'tv').length || 0,
+      person:
+        results?.filter((result) => result.media_type === 'person').length || 0
+    }
+  }, [results])
+
+  const resultsToDisplay = useMemo(() => {
+    switch (filter) {
+      default:
+      case 'all':
+        return results
+      case 'movie':
+        return results?.filter((result) => result.media_type === 'movie')
+      case 'tv':
+        return results?.filter((result) => result.media_type === 'tv')
+      case 'person':
+        return results?.filter((result) => result.media_type === 'person')
+    }
+  }, [filter])
+
+  const handleFilter = useCallback((filter?: Filters) => {
+    setFilter(filter)
+  }, [])
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -55,7 +79,8 @@ export const SearchProvider: React.FC = (props) => {
       value={{
         filter,
         searchText,
-        results,
+        results: resultsToDisplay,
+        mediaTypesCount,
         setSearchText,
         handleFilter,
         handleSearch,

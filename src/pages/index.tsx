be4a25a01ecd4a982f04de529d8f1bf711b8fa0f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 17:15:43 -0400

    refactor: remove dynamic filter from api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { GetServerSideProps } from 'next'
 import IndexTemplate, { IndexTemplateProps } from 'templates/index'
-import { Filters } from 'types/themoviedb-response'
 
 type IndexPageProps = IndexTemplateProps
 
@@ -12,14 +11,13 @@ export const getServerSideProps: GetServerSideProps<IndexPageProps> = async ({
   query
 }) => {
   const MOVIES_DB_API_KEY = process.env.MOVIES_DB_API_KEY
-  const API_URL = `https://api.themoviedb.org/3/search/${query.filter}?api_key=${MOVIES_DB_API_KEY}&query=${query.query}&page=1`
+  const API_URL = `https://api.themoviedb.org/3/search/multi?api_key=${MOVIES_DB_API_KEY}&query=${query.query}&page=1`
 
   const response = await fetch(API_URL)
   const data = await response.json()
 
   return {
     props: {
-      filter: query.filter as Filters,
       response: data
     }
   }

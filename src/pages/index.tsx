4f27c24a7e2791c63c17687b5e756b3eba5cb2eb
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-15 10:57:19 -0400

    feat: add getServerSideProps in IndexPage
    we can get dynamic results just passing different for query and search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
-import IndexTemplate from 'templates/index'
+import { GetServerSideProps } from 'next'
+import IndexTemplate, { IndexTemplateProps } from 'templates/index'
+import { Filters } from 'types/themoviedb-response'
 
-export default function IndexPage() {
-  return <IndexTemplate />
+type IndexPageProps = IndexTemplateProps
+
+export default function IndexPage(props: IndexPageProps) {
+  return <IndexTemplate {...props} />
+}
+
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async ({
+  query
+}) => {
+  const MOVIES_DB_API_KEY = process.env.MOVIES_DB_API_KEY
+  const API_URL = `https://api.themoviedb.org/3/search/${query.filter}?api_key=${MOVIES_DB_API_KEY}&query=${query.query}&page=1`
+
+  const response = await fetch(API_URL)
+  const data = await response.json()
+
+  return {
+    props: {
+      filter: query.filter as Filters,
+      response: data
+    }
+  }
 }

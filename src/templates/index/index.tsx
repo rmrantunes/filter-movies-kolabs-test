fc55c27926312553061214e5cf0288e5235f1614
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-16 11:51:54 -0400

    feat: add Cards with dynamic data

--- src/templates/index/index.tsx
+++ src/templates/index/index.tsx
@@ -1,17 +1,19 @@
+import { useEffect } from 'react'
+
 import Aside from 'components/Aside'
 import Card from 'components/Card'
 import { Container } from 'components/Container'
 import Search from 'components/Search'
 import { useSearch } from 'hooks/useSearch'
-import { useEffect } from 'react'
 import { MovieDBMultiResponse } from 'types/themoviedb-response'
+import { mapResultToCardProps } from 'utils/mapResultToCardProps'
 
 import * as S from './styles'
 
 export type IndexTemplateProps = { response: MovieDBMultiResponse }
 
 export default function IndexTemplate(props: IndexTemplateProps) {
-  const { setResults } = useSearch()
+  const { setResults, results } = useSearch()
 
   useEffect(() => {
     setResults(props.response.results)
@@ -23,12 +25,9 @@ export default function IndexTemplate(props: IndexTemplateProps) {
       <S.MainGrid>
         <Aside />
         <div>
-          <Card
-            title="Tom Holland"
-            subtitle="Acting"
-            image="https://image.tmdb.org/t/p/original/l6zPRmg8NI7Y65G5GUqwvjxFdsx.jpg"
-            description={`Conhecido por: 'Avengers: Infinity War', 'Avengers: Endgame'`}
-          />
+          {results?.map((result) => (
+            <Card key={result.id} {...mapResultToCardProps(result)} />
+          ))}
         </div>
       </S.MainGrid>
     </Container>

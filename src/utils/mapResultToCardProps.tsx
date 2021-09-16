63091c47bffa1e430731efdfc236ef580b2caf98
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-16 08:44:09 -0400

    feat: new util mapResultToCardProps

---
+++ src/utils/mapResultToCardProps.tsx
@@ -0,0 +1,33 @@
+import { CardProps } from 'components/Card'
+import { MediaType, MultiResult } from 'types/themoviedb-response'
+
+const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/original'
+
+export function mapResultToCardProps(result: MultiResult): CardProps {
+  switch (result.media_type) {
+    case MediaType.Movie:
+      return {
+        title: result.original_title,
+        subtitle: result.release_date,
+        description: result.overview,
+        image: MOVIE_DB_IMAGE_ENDPOINT + result.backdrop_path
+      }
+    case MediaType.Tv:
+      return {
+        title: result.name,
+        subtitle: result.first_air_date.toLocaleDateString(),
+        description: result.overview,
+        image: MOVIE_DB_IMAGE_ENDPOINT + result.backdrop_path
+      }
+    case MediaType.Person:
+      return {
+        title: result.name,
+        subtitle: result.known_for_department,
+        description: `Conhecido por: ${result.known_for
+          .map(({ title }) => (title ? `'${title}'` : ''))
+          .filter(Boolean)
+          .join(', ')}`,
+        image: MOVIE_DB_IMAGE_ENDPOINT + result.profile_path
+      }
+  }
+}

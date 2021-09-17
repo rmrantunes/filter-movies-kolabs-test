7cfe9cf6d633a2330b8204f61e825cca14d1cca7
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-17 08:42:18 -0400

    feat: add search with year shortchut feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next'
 import IndexTemplate, { IndexTemplateProps } from 'templates/index'
 import { responseMock } from 'templates/index/mock'
+import { extractYearFromQuery } from 'utils/extractYearFromQuery'
 
 type IndexPageProps = IndexTemplateProps
 
@@ -8,13 +9,18 @@ export default function IndexPage(props: IndexPageProps) {
   return <IndexTemplate {...props} />
 }
 
-export const getServerSideProps: GetServerSideProps<IndexPageProps> = async ({
-  query
-}) => {
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async (
+  ctx
+) => {
   const MOVIES_DB_API_KEY = process.env.MOVIES_DB_API_KEY
-  const API_URL = `https://api.themoviedb.org/3/search/multi?api_key=${MOVIES_DB_API_KEY}&query=${query.query}&page=1`
 
-  return { props: { response: responseMock } }
+  const { query, year } = extractYearFromQuery(ctx.query.query as string)
+
+  const API_URL =
+    `https://api.themoviedb.org/3/search/multi?api_key=${MOVIES_DB_API_KEY}&query=${query}&page=1` +
+    `${year ? `&year=${year}` : ''}`
+
+  // return { props: { response: responseMock } }
 
   const response = await fetch(API_URL)
   const data = await response.json()

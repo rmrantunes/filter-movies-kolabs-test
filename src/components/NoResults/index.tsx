e609e9337eb1adff707efee54ff8a014773b23be
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-17 17:04:16 -0400

    feat: add NoResults component

---
+++ src/components/NoResults/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export default function NoResults() {
+  return <S.NoResults>Nenhum resultado</S.NoResults>
+}
---
+++ src/components/NoResults/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const NoResults = styled.h2`
+  font-size: 1.25rem;
+  font-weight: 500;
+  margin: 0 auto;
+
+  width: max-content;
+`

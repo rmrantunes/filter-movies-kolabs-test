1c03a3357c6420b8245c5d9fc852c71d3e63dbaf
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-17 08:13:22 -0400

    refactor: remove isOpen from Modal inner code

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,7 +4,6 @@ import * as S from './styles'
 export type ModalProps = {
   children: React.ReactNode
   onClose?: () => void
-  isOpen?: boolean
 }
 
 export default function Modal(props: ModalProps) {
@@ -13,16 +12,9 @@ export default function Modal(props: ModalProps) {
   })
 
   return (
-    <>
-      {props.isOpen && (
-        <S.Wrapper>
-          <S.Overlay
-            onClick={props.onClose}
-            aria-label="Click to close modal"
-          />
-          <S.Modal>{props.children}</S.Modal>
-        </S.Wrapper>
-      )}
-    </>
+    <S.Wrapper>
+      <S.Overlay onClick={props.onClose} aria-label="Click to close modal" />
+      <S.Modal>{props.children}</S.Modal>
+    </S.Wrapper>
   )
 }

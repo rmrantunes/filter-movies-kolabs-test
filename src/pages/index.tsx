feb9309bdceacc5cc0cadaafb5570553a2d5c525
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-18 10:36:59 -0400

    refactor: remove IndexPage props & responseMock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import { GetServerSideProps } from 'next'
-import IndexTemplate, { IndexTemplateProps } from 'templates/index'
-import { responseMock } from 'templates/index/mock'
+import IndexTemplate from 'templates/index'
 import { MovieDBMultiResponse } from 'types/themoviedb-response'
 import { extractYearFromQuery } from 'utils/extractYearFromQuery'
 
 type IndexPageProps = { response: MovieDBMultiResponse }
 
-export default function IndexPage() {
+export default function IndexPage(props: IndexPageProps) {
   return <IndexTemplate />
 }
 
@@ -25,8 +24,6 @@ export const getServerSideProps: GetServerSideProps<IndexPageProps> = async (
   const API_URL =
     `https://api.themoviedb.org/3/search/multi?api_key=${MOVIES_DB_API_KEY}&query=${query}&page=1` +
     `${year ? `&year=${year}` : ''}`
-
-  // return { props: { response: responseMock } }
 
   const response = await fetch(API_URL)
   const data = await response.json()
